1db29c528d8f69060e2347b1b36b050bb69cf067
Piero Morais <piero.morais@gmail.com>
2024-11-27 21:26:47 -0300

    Compenents implementation
    CTransform - position, velocity, and angle. Based on Vec2 class.

--- headers/Components.h
+++ headers/Components.h
@@ -2,5 +2,22 @@
 #define COMPONENTS_HEADER
 
 // pure data components
+#include "Vec2.h"
+#include<SFML/Graphics.hpp>
+
+class CTransform
+{
+public:
+	Vec2	pos =	{0.0, 0.0};
+	Vec2	vel =	{0.0, 0.0};
+	float	angle = 0.0;
+
+	CTransform(const Vec2& p, const Vec2& v, float a)
+		:pos(p)
+		,vel(v)
+		,angle(a)
+	{}
+
+};
 
 #endif
