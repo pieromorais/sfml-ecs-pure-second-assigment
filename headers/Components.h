6c5af0bc2aee8a0e3fc5a6597db8f67d0d598c40
Piero Morais <piero.morais@gmail.com>
2024-11-27 21:33:20 -0300

    Components implementation
    CShape - based on sf::CircleShape from sfml

--- headers/Components.h
+++ headers/Components.h
@@ -20,4 +20,19 @@ public:
 
 };
 
+class CShape
+{
+public:
+	sf::CircleShape circle;
+
+	CShape(float radius, int points, const sf::Color& fill, const sf::Color& outline, float thickness)
+		:circle(radius, points)
+		{
+			circle.setFillColor(fill);
+			circle.setOutlineColor(outline);
+			circle.setOutlineThickness(thickness);
+			circle.setOrigin(radius, radius);
+		}
+};
+
 #endif
